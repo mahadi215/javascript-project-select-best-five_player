1c68f5209b6520952296fc03e59a857edc6e2d2f
mahadi215 <mahadihasanbd215@gmail.com>
2022-08-22 20:22:28 +0300

    html design

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementsByTagName('a').addEventListener('click', function(){
+    const playerSelected = target.parentNode;
+    console.log('helloooo');
+});
+document.getElementById('player-expense-btn').addEventListener('click', function(){
+ console.log('hello');
+});
